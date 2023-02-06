173e4052cf91614034de53b11fe1dea82d586f49
Christniumanfantonial <79451036+Christniumanfantonial@users.noreply.github.com>
2023-02-06 09:21:54 +0800

    Create fireconfig.js

---
+++ fireconfig.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAs_4imRLtnF4W_AuhoGWvrGAxzB44QiD4",
+    authDomain: "mycrudfirebase-5d241.firebaseapp.com",
+    databaseURL: "https://mycrudfirebase-5d241-default-rtdb.firebaseio.com",
+    projectId: "mycrudfirebase-5d241",
+    storageBucket: "mycrudfirebase-5d241.appspot.com",
+    messagingSenderId: "49285530741",
+    appId: "1:49285530741:web:9c07e372bbfbe3800ca43c",
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
